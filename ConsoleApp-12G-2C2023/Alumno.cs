5453414f5ac38bb6864590d70a43535a03339504
christian.slapak <christian.slapak@ort.edu.ar>
2023-10-02 20:58:08 -0300

    Segunda materia del alum no

--- ConsoleApp-12G-2C2023/Alumno.cs
+++ ConsoleApp-12G-2C2023/Alumno.cs
@@ -7,12 +7,14 @@ namespace ConsoleApp_12G_2C2023
     class Alumno:Persona
     {
         public int Legajo { get; set; }
-        public string Materia { get; set; }
+        public string Materia1 { get; set; }
+
+        public string Materia2 { get; set; }
 
         public new string MostrarDatos()
         {
             string retorno = "";
-            retorno = base.MostrarDatos() + " " + Legajo + " " + Materia;
+            retorno = base.MostrarDatos() + " " + Legajo + " " + Materia1 + " " + Materia2;
             return retorno;
         }
     }
